952ed249bf587cf2e0837280f7d5fa3b404c8d17
Alex Buob <alexander.buob@gmail.com>
2017-09-04 18:27:06 -0500

    initial commit

---
+++ brew_install.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+brew tap caskroom/cask
+brew tap homebrew/services
+
+#brew install bash
+#echo /usr/local/bin/bash >> /etc/shells
+#chsh -s /usr/local/bin/bash
+
+brew install dos2unix
+brew install wget
+brew install lftp
+#brew install curl
+
+#brew install python
+brew install perl
+brew install vim
+
+#brew install coreutils
+#brew install findutils
+#brew install gnu-sed
+#brew install gnu-tar
+#brew install gawk
+#brew install gnu-which
+#brew install grep
+
+brew install git
+brew install subversion
+
+brew install awscli
+
+brew install ant
+brew install maven
+brew install maven-completion
+
+brew install tomcat
+
+brew install mysql
+brew install postgresql
+
+brew install hadoop
+brew install hive
+brew install pig
+brew install apache-spark
+
+brew install elasticsearch
+brew install kibana
+
+brew install mobgodb
+brew install neo4j
+brew install cassandra
+brew install solr
+
+brew install zookeeper
+brew install kafka
+
+brew install jq
+
+brew install terraform
+brew install ansible
+
+#Cask installs
+brew install postman
+brew install sourcetree
+brew install textmate
+brew install devcenter
+brew install google-chrome
+brew install the-unarchiver
+
+brew install intellij-idea
+brew install datagrip
+
+brew install docker-toolbox
+brew install virtualbox
+brew install kitematic
+
+#Completion
+brew install bash-completion@2
+brew install docker-completion
+brew install docker-compose-completion
+brew install docker-machine-completion
